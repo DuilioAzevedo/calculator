935e336f58b1f05fca934906ebaaff466260b4d4
DuilioAzevedo <81258001+DuilioAzevedo@users.noreply.github.com>
2025-01-14 07:48:39 +0000

    function sub

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+
+
 function calcularSoma() {
     // Obter os valores dos inputs
     let num1 = parseFloat(document.getElementById("numero1").value);
     let num2 = parseFloat(document.getElementById("numero2").value);
-
     // Verificar se os valores são válidos
     if (isNaN(num1) || isNaN(num2)) {
         alert("Por favor, insira números válidos.");
@@ -14,4 +15,23 @@ function calcularSoma() {
 
     // Exibir o resultado
     document.getElementById("resultado").textContent = soma;
+}
+
+function calcularSub() {
+    
+    // Obter os valores dos inputs
+    let num1 = parseFloat(document.getElementById("numero1").value);
+    let num2 = parseFloat(document.getElementById("numero2").value);
+    
+    // Verificar se os valores são válidos
+    if (isNaN(num1) || isNaN(num2)) {
+        alert("Por favor, insira números válidos.");
+        return;
+    }
+
+    // Calcular a subtração
+    let sub = num1 - num2;
+
+    // Exibir o resultado
+    document.getElementById("resultado").textContent = sub;
 }
